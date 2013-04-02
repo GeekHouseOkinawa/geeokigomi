180e0603b72182c793f9661cfce8ae840ca5df6a
Seiei Higa <hanachin@gmail.com>
2013-04-02 21:34:41 +0900

    add tompng and update tweet text

--- geeokigomi.py
+++ geeokigomi.py
@@ -15,18 +15,18 @@ auth.set_access_token(access_token, access_secret)
 api = tweepy.API(auth_handler=auth)
 
 dt = datetime.date.today()
-string = u"@kyo2_ @cota2n @gliese035 @hanachin_ @kimihito_ @tomo_hiko "
+string = u"@tompng @kyo2_ @cota2n @gliese035 @hanachin_ @kimihito_ @tomo_hiko "
 
 if dt.weekday() == 0:
   string += u"今日は燃えるゴミの日ですよ！"
 elif dt.weekday() == 1:
-  string += u"今日は缶・ビンの燃えないゴミの日！"
+  string += u"今日は燃やさないゴミの日！"
 elif dt.weekday() == 2:
-  string += u"今日はペットボトルの日！"
+  string += u"今日は紙・布の日！"
 elif dt.weekday() == 3:
   string += u"今日は燃えるゴミの日ですよ！"
 elif dt.weekday() == 4:
-  string += u"今日は何の日かわかんない！"
+  string += u"今日は缶・ビン・ペットボトルの日!"
 elif dt.weekday() == 5: 
   string += u"ゴミはないけど、掃除しようぜ！"
 else:
